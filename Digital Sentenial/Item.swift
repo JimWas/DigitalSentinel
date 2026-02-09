6918d00b7f73f7a24f5dd16ca5ff406fe9574103
Jim Washkau <jim@jimwashkau.com>
2026-02-09 21:57:19 +0900

    Initial Commit

---
+++ Digital Sentenial/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Digital Sentenial
+//
+//  Created by Jim Washkau on 2/9/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
